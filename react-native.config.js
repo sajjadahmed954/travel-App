dada06d11b01b5792989971ce95765cc91d5632b
sajjadahmed954 <sajjadahmednizamani954@gmail.com>
2025-01-05 08:34:35 -0800

    Add files via upload

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dependencies: {
+      'react-native-document-picker': {
+        platforms: {
+          ios: null, // or "ios" if you want it to be available only for iOS
+        },
+      },
+    },
+  };
+  
